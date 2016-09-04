15b59371c51545672811fb0172c52cdf1a238adb
Juan Carlos Sastre <juan.carlos.sastre.iglesias@gmail.com>
2016-09-04 19:17:09 +0200

    SVM done.

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -19,11 +19,41 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
+ 
 
 
 #########################################################
 ### your code goes here ###
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+
+# features_train = features_train[:len(features_train)/100] 
+# labels_train = labels_train[:len(labels_train)/100] 
+
+clf = SVC(C=10000., kernel='rbf')
+
+print "Proceeding to train..." 
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+print "Proceeding to test..." 
+
+t1 = time()
+pred = clf.predict(features_test)
+print "predict time:", round(time()-t1, 3), "s"
+
+print pred.tolist().count(1)
+
+#print "Proceeding to calculate accuracy..." 
+
+# accuracy= accuracy_score(pred, labels_test)
+# print accuracy
+
+# pred = clf.predict([features_test[16], features_test[26], features_test[50]])
+# print pred
+
 
 #########################################################
 
